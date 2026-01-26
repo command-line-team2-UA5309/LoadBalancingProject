8264bebc676c20875ce82a337dd3f74276378812
DIMAX_BEDENKO <bedenko15@gmail.com>
2026-01-26 19:39:54 +0200

    added postgres.sh

---
+++ CL_Project-Dima/Set_up_PostgraSQL.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install curl ca-certificates
+sudo install -d /usr/share/postgresql-common/pgdg
+sudo curl -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc --fail https://www.postgresql.org/media/keys/ACCC4CF8.asc
+sudo sh -c "echo 'deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $VERSION_CODENAME-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+sudo apt update
+sudo apt -y install postgresql
+
+
