2d97b71bc5f5c07742f9264ead5844b53f77d28b
Vladyslav Buzhdyhan <vladyslavbuzhdyhan@gmail.com>
2026-01-28 14:11:43 +0200

    Change PostgreSQL script to set static ip

--- CL_Project-Vlad/scripts/postgresql.sh
+++ CL_Project-Vlad/scripts/postgresql.sh
@@ -1,18 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt install postgresql
+set -e
 
-sudo apt install nginx
+#Set static ip
+sudo tee /etc/netplan/01-netcfg.yaml << EOF
+network:
+  version: 2
+  ethernets:
+    enp0s3:
+      dhcp4: no
+      addresses:
+        - 10.0.2.6/24
+      routes:
+        - to: default
+          via: 10.0.2.1
+      nameservers:
+        addresses:
+          - 8.8.8.8
+          - 1.1.1.1
+EOF
+sudo chmod 600 /etc/netplan/01-netcfg.yaml
+sudo netplan apply
+
+
+#Install packages
+sudo apt install postgresql -y
+
+sudo apt install nginx -y
+
 
 #Lynis configuration
-
 sudo apt install lynis -y
 
 sudo mkdir /var/log/lynis
-sudo mkdir /usr/local/sbin/lynis
+sudo mkdir /usr/local/bin
 
-sudo tee /usr/local/sbin/lynis/cronjob.sh << 'EOF'
-#!/bin/sh
+sudo tee /usr/local/bin/lynis-cronjob.sh << 'EOF'
+#!/bin/bash
 
 set -u
 DATE=$(date +%Y%m%d_%H%M%S)
@@ -20,10 +44,10 @@ HOST="postgresql"
 REPORT="/var/log/lynis/report-${HOST}_${DATE}.txt"
 
 lynis audit system --cronjob > "${REPORT}"
-scp -o StrictHostKeyChecking=accept-new -i /home/nda/.ssh/host_machine "${REPORT}" nda@10.0.2.1:/home/nda/reports/
+sftp -o StrictHostKeyChecking=accept-new -i /home/nda/.ssh/host_machine "${REPORT}" nda@10.0.2.1:/home/nda/reports/
 EOF
-sudo chmod +x /usr/local/sbin/lynis/cronjob.sh
+sudo chmod +x /usr/local/bin/lynis-cronjob.sh
 
 sudo tee -a /etc/crontab << EOF
-0,30 * * * * root cd /usr/local/sbin/lynis && ./cronjob.sh
+0,30 * * * * root /usr/local/bin/lynis-cronjob.sh
 EOF
