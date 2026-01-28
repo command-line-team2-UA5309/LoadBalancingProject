dc281484af89f56f8b5b90804e29f053d2709768
Vladyslav Buzhdyhan <vladyslavbuzhdyhan@gmail.com>
2026-01-28 14:14:15 +0200

    Add configuration script for load balancer
    Add script for automatic configuration of load balancer VM.

---
+++ CL_Project-Vlad/scripts/load_balancer.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+
+#Set static ip
+sudo tee /etc/netplan/01-netcfg.yaml << EOF
+network:
+  version: 2
+  ethernets:
+    enp0s3:
+      dhcp4: no
+      addresses:
+        - 10.0.2.3/24
+      routes:
+        - to: default
+          via: 10.0.2.1
+      nameservers:
+        addresses:
+          - 8.8.8.8
+          - 1.1.1.1
+EOF
+sudo chmod 600 /etc/netplan/01-netcfg.yaml
+sudo netplan apply
+
+
+#Install packages
+sudo apt install nginx -y
+
+
+#Change default NGINX config
+sudo tee /etc/nginx/sites-available/default << EOF
+upstream backend {
+    server 10.0.2.4;
+    server 10.0.2.5;
+}
+
+server {
+    listen 80;
+
+    location / {
+        proxy_pass http://backend;
+    }
+}
+
+EOF
+sudo systemctl restart nginx
+
+
+#Lynis configuration
+
+sudo apt install lynis -y
+
+sudo mkdir /var/log/lynis
+sudo mkdir /usr/local/bin
+
+sudo tee /usr/local/bin/lynis-cronjob.sh << 'EOF'
+#!/bin/bash
+
+set -u
+DATE=$(date +%Y%m%d_%H%M%S)
+HOST="load_balancer"
+REPORT="/var/log/lynis/report-${HOST}_${DATE}.txt"
+
+lynis audit system --cronjob > "${REPORT}"
+sftp -o StrictHostKeyChecking=accept-new -i /home/nda/.ssh/host_machine "${REPORT}" nda@10.0.2.1:/home/nda/reports/
+EOF
+sudo chmod +x /usr/local/bin/lynis-cronjob.sh
+
+sudo tee -a /etc/crontab << EOF
+0,30 * * * * root /usr/local/bin/lynis-cronjob.sh
+EOF
