690c98f4729fb904494b3db4340aba5b3be0c6b2
Vladyslav Buzhdyhan <vladyslavbuzhdyhan@gmail.com>
2026-01-29 12:58:59 +0200

    CL-27 Add reading variables from .env file
    Add reading variables from .env file to choose which image we want to serve

--- CL_Project-Vlad/FlaskBirdApp/app.py
+++ CL_Project-Vlad/FlaskBirdApp/app.py
@@ -1,11 +1,15 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+bird_name = os.getenv("BIRD_NAME")
+location = os.getenv("BIRD_LOCATION")
+image = os.getenv("BIRD_IMAGE")
 
 @app.route('/')
 def main_page():
-    return render_template('index.html', name="Painted Bunting", location="Ixtapa, Mexico")
+    return render_template('index.html', name=bird_name, location=location, image=image)
 
 
 if __name__ == '__main__':
